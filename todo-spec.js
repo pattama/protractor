4dbf3b0e7f4d91e8d22f9057bf2717ace2d79a75
pattama <pattama.wara-asawapati@thomsonreuters.com>
2015-04-29 16:19:54 +0700

    initial commit for Protractor test

---
+++ todo-spec.js
@@ -0,0 +1,21 @@
+describe('safe login', function() {
+    it('should pass authenticaion and redirected to hannu dashboard', function() {
+
+        browser.ignoreSynchronization = true;
+        browser.get('https://compass.thomsonreuters.com/monitor/alarms/dashboard');
+        browser.findElement(by.id('USER')).sendKeys('8008788');
+        browser.findElement(by.id('PASSWORD')).sendKeys('2233Anowa#');
+        browser.findElement(by.id('safeLoginbtn')).click();
+
+        browser.wait(function() {
+            return browser.getCurrentUrl().then(function(url) {
+                return /dashboard/.test(url);
+            });
+        });
+
+        browser.sleep(5000);
+
+        expect(browser.getTitle()).toEqual('Compass Monitoring');
+    });
+
+});
