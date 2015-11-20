9e2bef56123919789e31551511a03029ae53b2e5
Pattama Wara-asawapati <pattama.wara-asawapati@thomsonreuters.com>
2015-11-20 18:11:59 +0700

    Update todo-spec.js

--- todo-spec.js
+++ todo-spec.js
@@ -3,8 +3,8 @@ describe('safe login', function() {
 
         browser.ignoreSynchronization = true;
         browser.get('https://compass.thomsonreuters.com/monitor/alarms/dashboard');
-        browser.findElement(by.id('USER')).sendKeys('8008788');
-        browser.findElement(by.id('PASSWORD')).sendKeys('2233Anowa#');
+        browser.findElement(by.id('USER')).sendKeys('xxxxx');
+        browser.findElement(by.id('PASSWORD')).sendKeys('xxxxxx');
         browser.findElement(by.id('safeLoginbtn')).click();
 
         browser.wait(function() {
